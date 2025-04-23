55e7710202d81a92744222575f4d6051e43a96eb
Raman Verma <ramanverma4183@gmail.com>
2025-04-23 09:31:05 +0530

    refactor(auth/redirect): move logic to the root of app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,9 @@
 import 'package:demo/src/core/constants/app_strings.dart';
+import 'package:demo/src/features/auth/auth.dart';
+import 'package:demo/src/features/home/home.dart';
 import 'package:demo/src/features/splash/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 class DemoApp extends StatelessWidget {
@@ -14,7 +17,25 @@ class DemoApp extends StatelessWidget {
         colorScheme: const ShadSlateColorScheme.light(),
         brightness: Brightness.light,
       ),
-      home: SplashScreen(),
+      home: Selector<AuthProvider, AuthState>(
+        builder: (context, state, child) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            final navigator = Navigator.of(context);
+
+            Future.delayed(const Duration(milliseconds: 400), () {
+              if (state == AuthState.signedOut) {
+                navigator.pushAndRemoveUntil(LogInScreen.route(), (_) => false);
+              } else if (state == AuthState.signedIn) {
+                navigator.pushAndRemoveUntil(HomeScreen.route(), (_) => false);
+              }
+            });
+          });
+
+          return child!;
+        },
+        selector: (context, provider) => provider.state,
+        child: SplashScreen(),
+      ),
     );
   }
 }
